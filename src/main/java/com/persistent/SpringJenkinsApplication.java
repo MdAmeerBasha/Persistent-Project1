e8b702991a67ba4072b68a99ea5d004283f574aa
Ameer Mohammad <ameer_mohammad@persistent.com>
2021-11-24 23:03:34 +0530

    changed

--- src/main/java/com/persistent/SpringJenkinsApplication.java
+++ src/main/java/com/persistent/SpringJenkinsApplication.java
@@ -10,7 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringJenkinsApplication {
 
-	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
 
 	@PostConstruct
 	public void init() {
